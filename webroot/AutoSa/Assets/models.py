7e563ac7eb2c7d5fe768b9d8a46f01323fc234e7
dalianlxw <dalianlxw@163.com>
2015-04-09 04:49:26 +0800

    commit jumpserver

---
+++ webroot/AutoSa/Assets/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from UserManage.models import User
+
+
+class IDC(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
+
+class SERV_STATUS(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
+
+class SERV_SORT(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Assets(models.Model):
+    id = models.AutoField(primary_key=True)
+    app = models.CharField(max_length=30)
+    sort = models.ForeignKey(SERV_SORT)
+    ip = models.CharField(max_length=20)
+    port = models.IntegerField(max_length=5)
+    idc = models.ForeignKey(IDC)
+    status = models.ForeignKey(SERV_STATUS)
+    type = models.CharField(max_length=20)
+    comment = models.CharField(max_length=100, blank=True, null=True)
+
+    def __unicode__(self):
+        return '%s ' % self.ip
+
+
+class AssetsUser(models.Model):
+    uid = models.ForeignKey(User)
+    aid = models.ForeignKey(Assets)
+
+
